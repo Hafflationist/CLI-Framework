c8f03978e5c61c5c76ee5c58013dd82b3e1702d2
hafflationist <hektor3.14159265358979@outlook.de>
2019-11-18 15:08:06 +0100

    updating to .net core 3.0

---
+++ ConsoleApplication/Program.fs
@@ -0,0 +1,19 @@
+﻿open System
+    
+    
+[<EntryPoint>]
+let main argv =   
+    Some 88
+    |> Option.filter (fun value -> value = 0 ) 
+    |> Option.defaultValue 44
+    |> Console.WriteLine
+    
+    
+    Console.ForegroundColor <- ConsoleColor.DarkYellow
+    Console.WriteLine "Hugobert in dunkelgelb"
+    printfn "haha test"
+    Console.ForegroundColor <- ConsoleColor.Yellow
+    Console.WriteLine "Hugobert in dunkelgelb"
+    
+    0 // return an integer exit code
+
